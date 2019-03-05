dc5309fe71f2dc7f37ac445baf847364d16e4f2c
Kolot Yuri <kolot.ius@students.dvfu.ru>
2019-03-05 21:27:49 +1000

    Create ListView with hours

--- app/src/main/java/com/github/royalstorm/android_task_manager/activity/DayActivity.java
+++ app/src/main/java/com/github/royalstorm/android_task_manager/activity/DayActivity.java
@@ -2,6 +2,8 @@ package com.github.royalstorm.android_task_manager.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.github.royalstorm.android_task_manager.R;
 
@@ -10,6 +12,19 @@ public class DayActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.week_activity);
+        setContentView(R.layout.day_activity);
+
+        showHours();
+    }
+
+    private void showHours() {
+        ListView hoursList = findViewById(R.id.hours);
+
+        String[] hours = getResources().getStringArray(R.array.hours);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, hours);
+
+        hoursList.setAdapter(adapter);
     }
 }
