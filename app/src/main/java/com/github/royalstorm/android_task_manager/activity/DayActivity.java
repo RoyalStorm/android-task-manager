cf51743c890340650501929fe298db51a01f1df0
Kolot Yuri <kolot.ius@students.dvfu.ru>
2019-03-05 20:18:32 +1000

    Add "day" and "week" layouts

---
+++ app/src/main/java/com/github/royalstorm/android_task_manager/activity/DayActivity.java
@@ -0,0 +1,15 @@
+package com.github.royalstorm.android_task_manager.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.royalstorm.android_task_manager.R;
+
+public class DayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.week_activity);
+    }
+}
