3eee1a847a17c25a38f9ac6d0c8fc04ae29db4a5
Kolot Yuri <kolot.ius@students.dvfu.ru>
2019-03-07 23:04:01 +1000

    Add day numbers for current week

--- app/src/main/java/com/github/royalstorm/android_task_manager/activity/WeekActivity.java
+++ app/src/main/java/com/github/royalstorm/android_task_manager/activity/WeekActivity.java
@@ -7,10 +7,13 @@ import android.widget.TextView;
 
 import com.github.royalstorm.android_task_manager.R;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
 public class WeekActivity extends AppCompatActivity {
+
+    private Calendar calendar = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +21,10 @@ public class WeekActivity extends AppCompatActivity {
         setContentView(R.layout.week_activity);
 
         setCurrentDay();
+        setDaysNumbers();
     }
 
     private void setCurrentDay() {
-        Calendar calendar = Calendar.getInstance();
-
         String currentDay = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault());
         String color = "#ff6f00";
 
@@ -40,7 +42,7 @@ public class WeekActivity extends AppCompatActivity {
                 break;
 
             case "Tuesday":
-                day = findViewById(R.id.thu);
+                day = findViewById(R.id.tue);
                 day.setTextColor(Color.parseColor(color));
                 break;
 
@@ -63,6 +65,50 @@ public class WeekActivity extends AppCompatActivity {
                 day = findViewById(R.id.sat);
                 day.setTextColor(Color.parseColor(color));
                 break;
+        }
+    }
+
+    private void setDaysNumbers() {
+        calendar.setFirstDayOfWeek(Calendar.MONDAY);
+        calendar.set(Calendar.DAY_OF_WEEK, calendar.getFirstDayOfWeek());
+
+        SimpleDateFormat dayNumber = new SimpleDateFormat("dd");
+
+        TextView number;
+
+        for (int i = 0; i < 7; i++) {
+            switch (i) {
+                case 0:
+                    number = findViewById(R.id.monNumber);
+                    number.setText(dayNumber.format(calendar.getTime()));
+                    break;
+                case 1:
+                    number = findViewById(R.id.tueNumber);
+                    number.setText(dayNumber.format(calendar.getTime()));
+                    break;
+                case 2:
+                    number = findViewById(R.id.wedNumber);
+                    number.setText(dayNumber.format(calendar.getTime()));
+                    break;
+                case 3:
+                    number = findViewById(R.id.thuNumber);
+                    number.setText(dayNumber.format(calendar.getTime()));
+                    break;
+                case 4:
+                    number = findViewById(R.id.friNumber);
+                    number.setText(dayNumber.format(calendar.getTime()));
+                    break;
+                case 5:
+                    number = findViewById(R.id.satNumber);
+                    number.setText(dayNumber.format(calendar.getTime()));
+                    break;
+                case 6:
+                    number = findViewById(R.id.sunNumber);
+                    number.setText(dayNumber.format(calendar.getTime()));
+                    break;
+            }
+
+            calendar.add(Calendar.DAY_OF_WEEK, 1);
         }
     }
 }
