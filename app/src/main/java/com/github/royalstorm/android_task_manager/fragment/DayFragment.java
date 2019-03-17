f6ed25865d250d373197d29e89ccd516802821d2
Kolot Yuri <kolot.ius@students.dvfu.ru>
2019-03-17 16:00:20 +1000

    Set adapter for hours list. Delete extra classes

--- app/src/main/java/com/github/royalstorm/android_task_manager/fragment/DayFragment.java
+++ app/src/main/java/com/github/royalstorm/android_task_manager/fragment/DayFragment.java
@@ -7,14 +7,34 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.github.royalstorm.android_task_manager.R;
 
 public class DayFragment extends Fragment {
 
+    private ListView hoursList;
+
+    private ArrayAdapter<String> adapter;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_day, container, false);
+        View view = inflater.inflate(R.layout.fragment_day,
+                container, false);
+
+        hoursList = view.findViewById(R.id.hoursList);
+        showHours();
+
+        return view;
+    }
+
+    private void showHours() {
+        String[] hours = getResources().getStringArray(R.array.hours);
+
+        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, hours);
+
+        hoursList.setAdapter(adapter);
     }
 }
