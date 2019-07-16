5c59f9470359c4682700ae153e6dcbaa91e8115b
Kolot Yuri <kolot.ius@students.dvfu.ru>
2019-07-16 14:45:55 +1000

    Add full permissions sharing

--- app/src/main/java/com/github/royalstorm/android_task_manager/fragment/dialog/AccessConfigurationDialog.java
+++ app/src/main/java/com/github/royalstorm/android_task_manager/fragment/dialog/AccessConfigurationDialog.java
@@ -9,7 +9,9 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.CheckBox;
 
+import com.annimon.stream.Stream;
 import com.github.royalstorm.android_task_manager.R;
 import com.github.royalstorm.android_task_manager.dao.Action;
 import com.github.royalstorm.android_task_manager.dao.EntityType;
@@ -18,18 +20,21 @@ import com.github.royalstorm.android_task_manager.dao.PermissionRequest;
 import com.github.royalstorm.android_task_manager.service.PermissionService;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AccessConfigurationDialog extends AppCompatDialogFragment
         implements PermissionService.RequestPermissionCallback {
+
+    private CheckBox cbRead;
+    private CheckBox cbUpdate;
+    private CheckBox cbDelete;
 
     private FirebaseAuth firebaseAuth;
     private String userToken;
     private PermissionService permissionService;
 
-    private Intent shareIntent;
-
     private ApplyAccessConfiguration applyAccessConfiguration;
-
-    PermissionRequest defaultPermission = new PermissionRequest();
 
     @NonNull
     @Override
@@ -39,6 +44,10 @@ public class AccessConfigurationDialog extends AppCompatDialogFragment
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.access_configuration_dialog, null);
 
+        cbRead = view.findViewById(R.id.cbRead);
+        cbUpdate = view.findViewById(R.id.cbUpdate);
+        cbDelete = view.findViewById(R.id.cbDelete);
+
         firebaseAuth = FirebaseAuth.getInstance();
         userToken = firebaseAuth.getCurrentUser().getIdToken(false).getResult().getToken();
         permissionService = new PermissionService(this);
@@ -46,24 +55,13 @@ public class AccessConfigurationDialog extends AppCompatDialogFragment
         Bundle accessConfigurationBundle = this.getArguments();
         EventInstance eventInstance = (EventInstance) accessConfigurationBundle.getSerializable(EventInstance.class.getSimpleName());
 
-        defaultPermission.setAction(Action.READ.name());
-        defaultPermission.setEntityType(EntityType.EVENT.name());
-        defaultPermission.setEntityId(eventInstance.getEventId());
-
         builder.setView(view)
                 .setTitle("Выберите уровни привилегий")
                 .setNegativeButton("Отмена", (dialogInterface, i) -> {
                 })
                 .setPositiveButton("Создать токен", (dialogInterface, i) -> {
-                    if (userToken == null) {
-                        firebaseAuth.getCurrentUser().getIdToken(true).addOnCompleteListener(task -> {
-                            userToken = task.getResult().getToken();
-                            permissionService.generateSharingLink(new PermissionRequest[]{defaultPermission}, userToken);
-                        });
-                    } else {
-                        userToken = firebaseAuth.getCurrentUser().getIdToken(false).getResult().getToken();
-                        permissionService.generateSharingLink(new PermissionRequest[]{defaultPermission}, userToken);
-                    }
+                    updateUserToken();
+                    permissionService.generateSharingLink(getSelectedPermissions(eventInstance), userToken);
                 });
 
         return builder.create();
@@ -83,14 +81,69 @@ public class AccessConfigurationDialog extends AppCompatDialogFragment
 
     @Override
     public void requestPermissionSuccess(boolean success, String sharingToken) {
-        shareIntent = new Intent();
-        shareIntent.setAction(Intent.ACTION_SEND);
-        shareIntent.putExtra(Intent.EXTRA_TEXT, sharingToken);
-        shareIntent.setType("text/plain");
-        applyAccessConfiguration.applySharingLink(shareIntent);
+        Intent shareWithFriendsIntent = new Intent();
+        shareWithFriendsIntent.setAction(Intent.ACTION_SEND);
+        shareWithFriendsIntent.putExtra(Intent.EXTRA_TEXT, sharingToken);
+        shareWithFriendsIntent.setType("text/plain");
+        applyAccessConfiguration.applySharingLink(shareWithFriendsIntent);
     }
 
     public interface ApplyAccessConfiguration {
         void applySharingLink(Intent sharingIntent);
+    }
+
+    private PermissionRequest[] getSelectedPermissions(EventInstance eventInstance) {
+        List<PermissionRequest> permissionRequests = new ArrayList<>();
+
+        if (cbRead.isChecked()) {
+            PermissionRequest readEventPermissionRequest = new PermissionRequest();
+            readEventPermissionRequest.setAction(Action.READ.name());
+            readEventPermissionRequest.setEntityType(EntityType.EVENT.name());
+            readEventPermissionRequest.setEntityId(eventInstance.getEventId());
+
+            permissionRequests.add(readEventPermissionRequest);
+        }
+
+        if (cbUpdate.isChecked()) {
+            PermissionRequest updateEventPermissionRequest = new PermissionRequest();
+            updateEventPermissionRequest.setAction(Action.UPDATE.name());
+            updateEventPermissionRequest.setEntityType(EntityType.EVENT.name());
+            updateEventPermissionRequest.setEntityId(eventInstance.getEventId());
+
+            PermissionRequest updateEventPatternPermissionRequest = new PermissionRequest();
+            updateEventPatternPermissionRequest.setAction(Action.UPDATE.name());
+            updateEventPatternPermissionRequest.setEntityType(EntityType.PATTERN.name());
+            updateEventPatternPermissionRequest.setEntityId(eventInstance.getPatternId());
+
+            permissionRequests.add(updateEventPermissionRequest);
+            permissionRequests.add(updateEventPatternPermissionRequest);
+        }
+
+        if (cbDelete.isChecked()) {
+            PermissionRequest deleteEventPermissionRequest = new PermissionRequest();
+            deleteEventPermissionRequest.setAction(Action.DELETE.name());
+            deleteEventPermissionRequest.setEntityType(EntityType.EVENT.name());
+            deleteEventPermissionRequest.setEntityId(eventInstance.getEventId());
+
+            PermissionRequest deleteEventPatternPermissionRequest = new PermissionRequest();
+            deleteEventPatternPermissionRequest.setAction(Action.DELETE.name());
+            deleteEventPatternPermissionRequest.setEntityType(EntityType.PATTERN.name());
+            deleteEventPatternPermissionRequest.setEntityId(eventInstance.getPatternId());
+
+            permissionRequests.add(deleteEventPermissionRequest);
+            permissionRequests.add(deleteEventPatternPermissionRequest);
+        }
+
+        return Stream.of(permissionRequests).toArray(PermissionRequest[]::new);
+    }
+
+    private void updateUserToken() {
+        if (userToken == null)
+            firebaseAuth.getCurrentUser().getIdToken(true).addOnCompleteListener(task -> {
+                userToken = task.getResult().getToken();
+            });
+        else
+            userToken = firebaseAuth.getCurrentUser().getIdToken(false).getResult().getToken();
+
     }
 }
